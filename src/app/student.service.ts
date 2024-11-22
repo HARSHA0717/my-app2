f3e69ed520912eba7cba3969303ad80b61d9b435
HARSHA0717 <harshamummadi0709@gmail.com>
2024-11-22 12:46:04 +0530

    creted sort and delete

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -14,4 +14,14 @@ export class StudentService {
   getfilterstudents(term:string):Observable<any>{
     return this._httpClient.get('https://6128991386a213001729f9df.mockapi.io/test/v1/student?filter='+term);
   }
+
+  getsortedstudents(column:string , order: string):Observable<any>{
+    return this._httpClient.get('https://6128991386a213001729f9df.mockapi.io/test/v1/student?sortBy='+column+'&order='+order);
+  }
+
+
+  deleteStudent(id:string):Observable<any>{
+    return this._httpClient.delete('https://6128991386a213001729f9df.mockapi.io/test/v1/student/'+id);
+  }
 }
+
